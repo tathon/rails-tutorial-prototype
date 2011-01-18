7b6f4878058c5bd8b58fb6ae2c4915be112310e5
Chu Tat Hon <chutathon@yahoo.com>
2011-01-17 19:51:55 -0800

    User model with passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,57 @@
 # == Schema Information
-# Schema version: 20110118023256
+# Schema version: 20110118031141
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  userid     :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  userid             :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
+require 'digest'
 class User < ActiveRecord::Base
-	attr_accessible :name, :userid
+	attr_accessor :password
+	attr_accessible :name, :userid, :password
 	
-	validates :name, 	:presence => true,
-										:length => { :maximum => 30 }
-	validates :userid,	:presence => true,
-											:length => { :maximum => 10 },
-											:uniqueness => { :case_sensitive => false }
+	validates :name, 			:presence => true,
+												:length => { :maximum => 30 }
+	validates :userid,		:presence => true,
+												:length => { :maximum => 10 },
+												:uniqueness => { :case_sensitive => false }
+	validates :password, 	:presence => true,
+												:length => { :within => 2..10 }
+												
+	before_save :encrypt_password
 	
+	def has_password?(submitted_password)
+		encrypted_password == encrypt(submitted_password)
+	end
+	
+	def self.authenticate(userid, submitted_password)
+		user = find_by_userid(userid)
+		return nil if user.nil?
+		return user if user.has_password?(submitted_password)
+	end
+	
+	private
+		def encrypt_password
+			self.salt = make_salt if new_record?
+			self.encrypted_password = encrypt(password)
+		end
+		
+		def encrypt(string)
+			secure_hash("#{salt}--#{string}")
+		end
+		
+		def make_salt
+			secure_hash("#{Time.now.utc}--#{password}")
+		end
+		
+		def secure_hash(string)
+			Digest::SHA2.hexdigest(string)
+		end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,11 @@ require 'spec_helper'
 
 describe User do
 	before(:each) do
-		@attr = { :name => "Test User", :userid => "testuser" }
+		@attr = {
+			:name => "Test User",
+			:userid => "testuser",
+			:password => "foobar"
+			}
 	end
 
 	it "should create a new instance given valid attributes" do
@@ -36,5 +40,64 @@ describe User do
 		User.create!(@attr.merge(:userid => upcased_userid))
 		user_with_duplicated_userid = User.new(@attr)
 		user_with_duplicated_userid.should_not be_valid
+	end
+	
+	describe "password validations" do
+		it "should require a password" do
+			User.new(@attr.merge(:password => "")).should_not be_valid
+		end
+				
+		it "should reject short password" do
+			short = "a" * 1
+			hash = @attr.merge(:password => short)
+			User.new(hash).should_not be_valid		
+		end
+		
+		it "should reject long passwords" do
+			long = "a" * 11
+			hash = @attr.merge(:password => long)
+			User.new(hash).should_not be_valid
+		end
+	end
+	
+	describe "password encryption" do
+		before(:each) do
+			@user = User.create!(@attr)
+		end
+		
+		it "should have an encrypted password attribute" do
+			@user.should respond_to(:encrypted_password)
+		end
+		
+		it "should set the encrypted password" do
+			@user.encrypted_password.should_not be_blank
+		end
+		
+		describe "has_password? method" do
+			it "should be true if the password match" do
+				@user.has_password?(@attr[:password]).should be_true
+			end
+			
+			it "should be false if the password don't match" do
+				@user.has_password?("invalid").should be_false
+			end
+		end
+		
+		describe "authenticate method" do
+			it "should return nil on userid/password mismatch" do
+				wrong_password_user = User.authenticate(@attr[:userid], "wrongpass")
+				wrong_password_user.should be_nil
+			end
+			
+			it "should return nil for an userid with no user" do
+				nonexistent_user = User.authenticate("wronguser", @attr[:password])
+				nonexistent_user.should be_nil
+			end
+			
+			it "should return the user on userid/password match" do
+				matching_user = User.authenticate(@attr[:userid], @attr[:password])
+				matching_user.should == @user
+			end
+		end
 	end
 end
